baa56f42457376b80d34b7ccff56a8086af4f71e
Ihsen <bouallegueihsen@gmail.com>
2023-03-04 00:20:39 +0100

    add sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
+import Client from "../ui/sections/Client";
+import Hero from "../ui/sections/Hero";
+import Lessons from "../ui/sections/Lessons";
+import Problem from "../ui/sections/Problem";
+import Project from "../ui/sections/Project";
+import Team from "../ui/sections/Team";
+import Tech from "../ui/sections/Tech";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Hero />
+      <Team />
+      <Client />
+      <Problem />
+      <Project />
+      <Tech />
+      <Lessons />
+    </>
+  );
 }
